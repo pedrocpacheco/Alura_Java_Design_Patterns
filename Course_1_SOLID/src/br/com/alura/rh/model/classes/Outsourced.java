950c1aa7ac9c01e42305b8550a299fc9cfd9537d
pedrocpacheco <cpedropacheco@hotmail.com>
2023-04-18 13:37:38 -0300

    feat(classes.Outsourced): adding getters and setters

--- Course_1_SOLID/src/br/com/alura/rh/model/classes/Outsourced.java
+++ Course_1_SOLID/src/br/com/alura/rh/model/classes/Outsourced.java
@@ -4,9 +4,18 @@ import java.math.BigDecimal;
 
 public class Outsourced extends Employee{
 
+    private String company;
+
     public Outsourced(String name, String cpf, Role role, BigDecimal salary) {
         super(name, cpf, role, salary);
-        //TODO Auto-generated constructor stub
     }
-    
+   
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
 }
