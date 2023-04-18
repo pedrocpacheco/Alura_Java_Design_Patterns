7a3fc98b4a84edbed49f89d827a58f1873ad744f
pedrocpacheco <cpedropacheco@hotmail.com>
2023-04-18 13:56:14 -0300

    refactor(classes.Employee): removing the setters

--- Course_1_SOLID/src/br/com/alura/rh/model/classes/Employee.java
+++ Course_1_SOLID/src/br/com/alura/rh/model/classes/Employee.java
@@ -20,45 +20,25 @@ public class Employee {
 	public void promote(Role role){
 		this.personalData.setRole(role);
 	}
-
+	
 	public String getName() {
 		return this.personalData.getName();
 	}
-
-	public void setName(String nome) {
-		this.personalData.setName(nome);
-	}
-
+	
 	public String getCpf() {
 		return this.personalData.getCpf();
 	}
-
-	public void setCpf(String cpf) {
-		this.personalData.setCpf(cpf);;
-	}
-
+	
 	public Role getRole() {
 		return this.personalData.getRole();
-	}
-
-	public void setRole(Role cargo) {
-		this.personalData.setRole(cargo);;
 	}
 
 	public BigDecimal getSalary() {
 		return this.personalData.getSalary();
 	}
 
-	public void setSalary(BigDecimal salario) {
-		this.personalData.setSalary(salario);;
-	}
-
 	public LocalDate getDateLastReajust() {
 		return dateLastReajust;
-	}
-
-	public void setDateLastReajust(LocalDate dataUltimoReajuste) {
-		this.dateLastReajust = dataUltimoReajuste;
 	}
 
 }
