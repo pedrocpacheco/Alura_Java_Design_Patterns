de8dd75498509a152e88bb27668a6e28921aa9ab
pedrocpacheco <cpedropacheco@hotmail.com>
2023-04-18 13:36:19 -0300

    feat(classes): starting a Outsourced Class

---
+++ Course_1_SOLID/src/br/com/alura/rh/model/classes/Outsourced.java
@@ -0,0 +1,12 @@
+package br.com.alura.rh.model.classes;
+
+import java.math.BigDecimal;
+
+public class Outsourced extends Employee{
+
+    public Outsourced(String name, String cpf, Role role, BigDecimal salary) {
+        super(name, cpf, role, salary);
+        //TODO Auto-generated constructor stub
+    }
+    
+}
